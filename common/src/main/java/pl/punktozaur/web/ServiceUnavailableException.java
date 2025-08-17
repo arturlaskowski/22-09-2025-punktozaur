e88b7bbfbafc8c4b1fe253ebb94f996cc8d57b71
Artur Laskowski <artur.laskowski@comarch.com>
2025-08-17 14:33:23 +0200

    zad2-start

---
+++ common/src/main/java/pl/punktozaur/web/ServiceUnavailableException.java
@@ -0,0 +1,12 @@
+package pl.punktozaur.web;
+
+public class ServiceUnavailableException extends RuntimeException {
+
+    public ServiceUnavailableException(String serviceName, String operation, Throwable cause) {
+        super(String.format("%s service is unavailable while %s", serviceName, operation), cause);
+    }
+
+    public ServiceUnavailableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
